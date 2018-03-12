9b80496c9bf68f7f696ce5b234b98bc3772a9feb
Matthew Wilkes <tonicblue@gmail.com>
2018-03-12 12:38:15 +0000

    Started refactor (typescriptification) work

---
+++ test/test.js
@@ -0,0 +1,21 @@
+const pa = require('path');
+const fs = require('fs');
+
+const assert = require('assert');
+const JsonInc = require('../dist/index').JsonInc;
+
+var jsonInc = new JsonInc({});
+
+var test1Path = pa.join(process.cwd(), 'test-data/test1.inc.json');
+var test1Dir = pa.dirname(test1Path);
+var test1Json = fs.readFileSync(test1Path).toString();
+var test1Expected = fs.readFileSync(test1Path + '-expected').toString();
+
+describe('JsonInc', function() {
+    describe('#transpile()', function() {
+        it('should return a match to expected outcome for test1.inc.json', function() {
+            var output = jsonInc.transpile(test1Json, test1Dir);
+            assert.equal(output, test1Expected);
+        });
+    });
+});
